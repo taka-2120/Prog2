66c5864c3dfe8ea298a5ddb50710a22fe0814eb7
taka-2120 <yu_tkhs@icloud.com>
2023-04-25 12:01:58 +0900

    remove: comments

--- t3/prac/e3_9.c
+++ t3/prac/e3_9.c
@@ -2,23 +2,23 @@
 
 int main()
 {
-  int num = 42;            // 正解の値
-  int point = 5;           // 初期持ち点
-  int answer = 0;          // ユーザの入力
-  int incorrect_count = 0; // 不正解の回数
+  int num = 42;            
+  int point = 5;           
+  int answer = 0;          
+  int incorrect_count = 0; 
 
-  // ゲームのループ
+  
   while (1)
   {
-    // 入力の受け取り
+    
     printf("数字を入力してください: ");
     scanf("%d", &answer);
 
-    // 正解の判定
+    
     if (answer == num)
     {
       printf("大正解、%d点です!\n", point);
-      break; // ゲーム終了
+      break; 
     }
     else if (answer > num)
     {
@@ -29,12 +29,12 @@ int main()
       printf("小さいです!\n");
     }
 
-    // 不正解回数のカウントと減点
+    
     incorrect_count++;
     if (incorrect_count >= 5)
     {
       printf("0点です!\n");
-      break; // ゲーム終了
+      break; 
     }
     else
     {
